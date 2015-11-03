dd1d415b3b5a688d39d43f4ed5f11f03607f718b
AnhKhoa <dnanhkhoa@live.com>
2015-11-03 23:57:01 +0700

    Added rule 3.1.f + test file

---
+++ rule_3_1_f_test.c
@@ -0,0 +1,41 @@
+/* flag ? 10 : 20 */
+// flag ? 10 : 20
+a = (flag ? 10 : 20); // flag?10:20
+
+a = (flag ? x = 40 : 20); // flag?10:20
+
+a = /* flag ? 10 : 20 */ flag?0:1; // Error
+
+/*
+	Don't check
+
+ 	a = flag?  0:     1
+ */
+a = flag            ?  0:               1  ; // Error
+
+x = "(flag ? 10 : 20)"; // OK
+
+a = flag ? flag ? 3 :5 :9; // Error
+
+a = flag ? flag ? 3 : 5 : 9; // OK
+
+a = flag ? 5 : flag ? 2 : 2; // OK
+
+a = flag ?5 :flag ? 2 : 2; // Error
+
+a = flag ? 1 : 2 ? 2 : 3; // OK
+
+a = flag ? 1 :2 ?2 : 3;  // Error
+
+
+a = flag ? flag ? 10 : 20 ? 3 : 5 : 9; // OK
+
+x = "a = flag            ?  0:               1 "; // OK
+
+switch(5)
+{
+	case 1: // OK
+		break;
+	case 2: // OK
+		break;
+}
