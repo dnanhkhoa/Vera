ae29a265dfdd0cd10edaff084479a22f270da7ec
AnhKhoa <dnanhkhoa@live.com>
2015-11-04 00:09:31 +0700

    Fixed rule_3_1_f_test.c

--- rule_3_1_f_test.c
+++ rule_3_1_f_test.c
@@ -1,10 +1,10 @@
-/* flag ? 10 : 20 */
-// flag ? 10 : 20
+/* flag ? 10 : 20	Don't check */
+// flag ? 10 : 20	Don't check
 a = (flag ? 10 : 20); // flag?10:20
 
 a = (flag ? x = 40 : 20); // flag?10:20
 
-a = /* flag ? 10 : 20 */ flag?0:1; // Error
+a = /* flag ? 10 : 20 Don't check */ flag?0:1; // Error
 
 /*
 	Don't check
@@ -13,7 +13,7 @@ a = /* flag ? 10 : 20 */ flag?0:1; // Error
  */
 a = flag            ?  0:               1  ; // Error
 
-x = "(flag ? 10 : 20)"; // OK
+x = "(flag ? 10 : 20)"; // Don't check
 
 a = flag ? flag ? 3 :5 :9; // Error
 
@@ -30,12 +30,12 @@ a = flag ? 1 :2 ?2 : 3;  // Error
 
 a = flag ? flag ? 10 : 20 ? 3 : 5 : 9; // OK
 
-x = "a = flag            ?  0:               1 "; // OK
+x = "a = flag            ?  0:               1 "; // Don't check
 
 switch(5)
 {
-	case 1: // OK
+	case 1: // Don't check
 		break;
-	case 2: // OK
+	case 2: // Don't check
 		break;
 }
